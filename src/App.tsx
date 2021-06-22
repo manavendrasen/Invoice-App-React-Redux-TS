a611bbd3859624c88351be3498a4e6bc12148b05
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-06-22 21:10:13 +0530

    build: add router and setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-// import { Counter } from './features/counter/Counter';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// views
+import { Dashboard, InvoiceForm } from './views';
 
 const App = () => {
-  return <div>hello</div>
+  return <Router><Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/new" exact component={InvoiceForm} />
+  </Switch></Router>
 }
 
 export default App;
---
+++ src/views/index.tsx
@@ -0,0 +1,4 @@
+import Dashboard from "./Dashboard";
+import InvoiceForm from './InvoiceForm'
+
+export { Dashboard, InvoiceForm }
